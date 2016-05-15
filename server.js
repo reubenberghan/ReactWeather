9f9aecb906142000c7ee2c1a86fd5697ca7fbca4
Reuben Berghan <reuben.berghan@gmail.com>
2016-05-16 07:02:36 +1000

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ const PORT = process.env.PORT || 3000;
 // set the port
 app.set('port', PORT);
 
+// custom middleware to check all incoming requests
+// if `http` then fine else redirect as `http`
+// this is because openweathermap doesn't like `https` requests
+app.use(function httpsRedirect (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 // set our folder to serve static files from
 app.use(express.static('public'));
 
