daf566b6b5197e49f0d9d9cd257e6510ba06a4db
Reuben Berghan <reuben.berghan@gmail.com>
2016-05-17 07:40:40 +1000

    Add styles to Main & Examples components

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,14 +1,23 @@
 'use strict';
 
 var React = require('react');
+var { Link } = require('react-router');
 
 // using reacts new stateless functional components feature
 // see `About.jsx` for more notes
 var Examples = props => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to the examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Sydney">Sydney, NSW</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Wellington">Wellington, NZ</Link>
+                </li>
+            </ol>
         </div>
     );
 };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -3,6 +3,21 @@
 var React = require('react');
 var Nav = require('Nav');
 
+// refactor to be a stateless functional component
+// note that as `props` is passed in as a parameter we no longer use the `this` as we do above
+var Main = props => {
+    return (
+        <div>
+            <Nav />
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    { props.children }
+                </div>
+            </div>
+        </div>
+    );
+};
+
 // using the `this.props.children` object we allow `react` and `react-router` to render the childern component of the respective route we want to access
 // var Main = React.createClass({
 //     render: function renderMain () {
@@ -15,17 +30,5 @@ var Nav = require('Nav');
 //         );
 //     }
 // });
-
-// refactor to be a stateless functional component
-// note that as `props` is passed in as a parameter we no longer use the `this` as we do above
-var Main = props => {
-    return (
-        <div>
-            <Nav />
-            <h2>Main Component</h2>
-            { props.children }
-        </div>
-    );
-};
 
 module.exports = Main;
